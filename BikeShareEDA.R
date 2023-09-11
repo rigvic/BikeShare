88ae6f0aabcf21d850577c76e3c5d0c59cd6de27
Victoria Rigby <victoriarigby@Victorias-MacBook-Pro-3.local>
2023-09-11 13:43:05 -0600

    BikeShare Analysis

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -2,4 +2,13 @@
 
 library(tidyverse)
 library(vroom)
+library(patchwork)
 bike <- vroom("./train.csv")
+
+dplyr::glimpse(bike)
+plot1 <- DataExplorer::plot_intro(bike)
+plot2 <- DataExplorer::plot_correlation(bike)
+plot3 <- ggplot(data=bike, aes(x=temp, y=count)) + geom_point() +geom_smooth(se=FALSE)
+plot4 <- DataExplorer::plot_missing(bike)
+GGally::ggpairs(bike)
+(plot1 + plot2) / (plot4 + plot3)
